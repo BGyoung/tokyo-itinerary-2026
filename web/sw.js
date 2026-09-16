115fcdc8ae2c1dca941ceca181a6b478acf0985d
BGyoung <ben7965@gmail.com>
2026-09-17 05:33:15 +0800

    Fix mobile ticket guide layout and refresh cache

--- web/sw.js
+++ web/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "tokyo-itinerary-v4";
+const CACHE_NAME = "tokyo-itinerary-v5";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260917-7",
-  "./app.js?v=20260917-7",
+  "./styles.css?v=20260917-8",
+  "./app.js?v=20260917-8",
   "./manifest.webmanifest",
   "./icon.svg"
 ];
