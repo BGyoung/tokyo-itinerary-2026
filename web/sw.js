1261c6c9078f75d10233a55a1f683a39972fe107
BGyoung <ben7965@gmail.com>
2026-09-17 01:14:37 +0800

    Add prominent offline status light

--- web/sw.js
+++ web/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "tokyo-itinerary-v2";
+const CACHE_NAME = "tokyo-itinerary-v3";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260917-5",
-  "./app.js?v=20260917-5",
+  "./styles.css?v=20260917-6",
+  "./app.js?v=20260917-6",
   "./manifest.webmanifest",
   "./icon.svg"
 ];
