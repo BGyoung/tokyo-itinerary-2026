517bb92385ddb0488e12f68cc2e1a3a463e4227f
BGyoung <ben7965@gmail.com>
2026-09-17 05:35:02 +0800

    Fix mobile anchor spacing for ticket guide

--- web/sw.js
+++ web/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "tokyo-itinerary-v5";
+const CACHE_NAME = "tokyo-itinerary-v6";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260917-8",
+  "./styles.css?v=20260917-9",
   "./app.js?v=20260917-8",
   "./manifest.webmanifest",
   "./icon.svg"
